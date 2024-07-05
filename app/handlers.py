c46084922abccaade0c95dab29a6dde24b31ed4a
mshavrov <mshavrov@gmail.com>
2024-07-05 14:01:17 +0300

    Обновление

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,6 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 import app.keyboards as kb
 
@@ -9,12 +9,17 @@ router = Router()
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     # здороваемся, узнаем сразу ID и первое имя
-    await message.reply(f'Банжорно. \nТвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}',
-                        reply_markup=await kb.inline_cars())
+    await message.reply(f'Привет {message.from_user.first_name} {message.from_user.last_name}!\nТвой ID: {message.from_user.id} \nТвой  ник:  @{message.from_user.username}',
+                        reply_markup=kb.main)
+                        # reply_markup=await kb.inline_orders())
     
 @router.message(Command('down_list'))
 async def get_help(message: Message):
     await message.answer('Загружаем список накладных!)))')
+
+@router.message(Command('nakladnaya'))
+async def get_help(message: Message):
+    await message.answer('Одна !)))')
 
 @router.message(Command('redy_list'))
 async def get_help(message: Message):
@@ -22,8 +27,26 @@ async def get_help(message: Message):
 
 @router.message(F.text == 'Обед')
 async def get_otdjh(message: Message):
-    await message.answer('Рано отдыхать - работай!!!)))')
+    await message.answer('Иди ты найху! \n\n работай!!!')
 
 @router.message(F.photo)
 async def get_photo(message: Message):
     await message.answer(f'ID фото {message.photo[-1].file_id}')
+    
+
+@router.callback_query(F.data == 'down_list')
+async def down_list(callback:CallbackQuery):
+    await callback.answer('Загружаем список накладных для сборки!')
+    await callback.message.edit_text("Список накладных для сборки.", reply_markup=await kb.inline_orders())
+    
+
+@router.callback_query(F.data == 'nakladnaya')
+async def down_list(callback:CallbackQuery):
+    await callback.answer('')
+    await callback.message.answer("Открываем список накладных")
+    
+
+@router.callback_query(F.data == 'redy_list')
+async def down_list(callback:CallbackQuery):
+    await callback.answer('')
+    await callback.message.answer("Список готовых накладных...")
--- app/keyboards.py
+++ app/keyboards.py
@@ -2,12 +2,22 @@ from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
                            InlineKeyboardMarkup, InlineKeyboardButton)
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='1. Загрузи накладные для работы')],
-    [KeyboardButton(text='2. Сборка по накладным'), KeyboardButton(text='3. Собранные накладные')],
+# Обычная клавиатура 
+# main = ReplyKeyboardMarkup(keyboard=[
+#     [KeyboardButton(text='1. Загрузи накладные для работы')],
+#     [KeyboardButton(text='2. Сборка по накладным'), KeyboardButton(text='3. Собранные накладные')],
+# ],
+#                            resize_keyboard=True,
+#                            input_field_placeholder='Жмакни одну из клавиш')
+
+# Клавиатура с callback
+main = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Загрузи накладные для работы', callback_data='down_list')],
+    [InlineKeyboardButton(text='Сборка по накладным', callback_data='nakladnaya'), InlineKeyboardButton(text='Собранные накладные', callback_data='redy_list')],
 ],
                            resize_keyboard=True,
                            input_field_placeholder='Жмакни одну из клавиш')
+
 
 settings = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='YA_COM', url='https://ya.com')],
@@ -21,7 +31,8 @@ settings = InlineKeyboardMarkup(inline_keyboard=[
     InlineKeyboardButton(text='YA_YA', url='https://yandex.ru'), InlineKeyboardButton(text='YA_RU', url='https://ya.ru')]
     ])
 
-cars = ['Купчино', 'Звёздная', 'Московская', 'Парк Победы', 'Электросила', 'Московские Ворота', 'Фрунзенская', 'Технологический Институт', 'Садовая', 'Невский Проспект', 'Горьковская', 'Петроградская', 'Черная речка', 'Пионерская', 'Удельная', 'Озерки', 'Проспект Просвящения', 'Парнас']
+# cars 
+orders = ['0юГЛ-011339', '0юГЛ-011340', '0юГЛ-011341', '0юГЛ-011342', '0юГЛ-011343', '0юГЛ-011344', '0юГЛ-011345', '0юГЛ-011346', '0юГЛ-011347', '0юГЛ-011348', '0юГЛ-011349', '0юГЛ-011350', '0юГЛ-011351', '0юГЛ-011352', '0юГЛ-011353', '0юГЛ-011354', '0юГЛ-011355', '0юГЛ-011356', '0юГЛ-011357', '0юГЛ-011358', '0юГЛ-011359', '0юГЛ-011360', '0юГЛ-011361', '0юГЛ-011362', '0юГЛ-011363', '0юГЛ-011364', '0юГЛ-011365', '0юГЛ-011366', '0юГЛ-011367', '0юГЛ-011368', ]
 
 # Меню внизу
 # async def inline_cars():
@@ -31,8 +42,9 @@ cars = ['Купчино', 'Звёздная', 'Московская', 'Парк 
 #     return keyboard.adjust(3).as_markup()
 
 # Меню у сообщения
-async def inline_cars():
+# async def inline_cars():
+async def inline_orders():
     keyboard = InlineKeyboardBuilder()
-    for car in cars:
-        keyboard.add(InlineKeyboardButton(text=car, url='https://ya.com'))
-    return keyboard.adjust(3).as_markup()
+    for order in orders:
+        keyboard.add(InlineKeyboardButton(text=order, url='https://ya.com'))
+    return keyboard.adjust(1).as_markup()
